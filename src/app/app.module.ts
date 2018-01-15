74131ec54c3c62ff8512bf97b8563dbb0f21f77a
siojurnalispipin <siojurnalispipin@hotmail.com>
2018-01-15 23:34:26 +0700

    data terhubung dengan resfull laravel

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,6 +9,7 @@ import { LoginPage } from './../pages/login/login';
 
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
+import { HttpModule } from '@angular/http';
 
 @NgModule({
   declarations: [
@@ -20,6 +21,7 @@ import { SplashScreen } from '@ionic-native/splash-screen';
   imports: [
     BrowserModule,
     IonicModule.forRoot(MyApp),
+    HttpModule
   ],
   bootstrap: [IonicApp],
   entryComponents: [
--- src/pages/login/login.ts
+++ src/pages/login/login.ts
@@ -1,12 +1,7 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
-
-/**
- * Generated class for the LoginPage page.
- *
- * See https://ionicframework.com/docs/components/#navigation for more info on
- * Ionic pages and navigation.
- */
+import { Http, Headers } from '@angular/http';
+import 'rxjs/add/operator/map';
 
 @IonicPage()
 @Component({
@@ -15,11 +10,29 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 })
 export class LoginPage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
-  }
+  url : string = "http://localhost:8000/api/tasks";
+  ini : string;
+
+  constructor(
+    public navCtrl: NavController,
+    public navParams: NavParams,
+    private http : Http
+  ) { }
 
   ionViewDidLoad() {
-    console.log('ionViewDidLoad LoginPage');
+    this.muatBerita();
+  }
+
+
+  muatBerita(){
+    this.http.get(this.url)
+    .map( res => res.json())
+    .subscribe(ini => {
+      this.ini = ini.data;
+      console.log(ini.data);
+    }, err =>{
+      console.log(err);
+    });
   }
 
 }
